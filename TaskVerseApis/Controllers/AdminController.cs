4e01f089f501e55e368ddc06b300be564f004d8f
manishpachauri18 <panditmanish1437@gmail.com>
2025-11-07 15:43:34 +0530

    checking authorization

--- TaskVerseApis/Controllers/AdminController.cs
+++ TaskVerseApis/Controllers/AdminController.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace TaskVerseApis.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class AdminController : ControllerBase
     {
+
+        [HttpGet]
+        protected string GetInfo()
+        {
+            return "Only Authorised By the Admin Policy";
+        }
     }
 }
